28d264fc33ff7b78e1f63dfed12eeb5dd203d90a
Ibrahim Šuta <ibro@ibro.me>
2016-03-28 09:19:00 +0200

    Overriding default onSubmit event

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -13,9 +13,13 @@ export default class SearchBar extends Component {
         this.setState({term: event.target.value});
     }
 
+    onFormSubmit(event){
+        event.preventDefault();
+    }
+
     render() {
         return (
-            <form className="input-group">
+            <form onSubmit={this.onFormSubmit} className="input-group">
                 <input value={this.state.term}
                        onChange={this.onInputChange}
                        className="form-control"
