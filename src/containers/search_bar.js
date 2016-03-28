64d8d8f08c1acbf749a89522d0b2cdd2033548ac
Ibrahim Šuta <ibro@ibro.me>
2016-03-28 10:16:02 +0200

    Connecting SearchBar with Redux store (calling action creator fetchWeather)

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+
+class SearchBar extends Component {
     constructor(props) {
         super(props);
 
         this.state = {term: ''};
 
         this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     onInputChange(event) {
         this.setState({term: event.target.value});
     }
 
-    onFormSubmit(event){
+    onFormSubmit(event) {
         event.preventDefault();
+
+        // call API - action creator fetchWeather
+        this.props.fetchWeather(this.state.term);
+
+        // clear search input
+        this.setState({term: ''});
     }
 
     render() {
@@ -32,3 +43,15 @@ export default class SearchBar extends Component {
         );
     }
 }
+
+
+function mapDispatchToProps(dispatch) {
+    var actionCreators = {fetchWeather: fetchWeather};
+
+    // Turns an object whose values are action creators, into an object with the same keys,
+    // but with every action creator wrapped into a dispatch call so they may be invoked directly.
+    return bindActionCreators(actionCreators, dispatch);
+}
+
+// Connects a React component to a Redux store.
+export default connect(null, mapDispatchToProps)(SearchBar);
